c8f36b068e49e329a6cda18e89b6c7bc65887add
Connor Cacy <connorecacy@gmail.com>
2024-11-26 13:14:28 -0600

    removed ping notification on world map

---
+++ ValheimModNoCompass/ChatPing.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace ValheimModNoCompass
+{
+    [HarmonyPatch(typeof(Chat), "SendPing")]
+    public class ChatPing
+    {
+        
+        [HarmonyPrefix]
+        public static bool Prefix()
+        {
+            return false;
+        }
+
+    }
+}
