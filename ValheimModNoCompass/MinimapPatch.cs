5702c20e9cee378c6217f83f3686c97b2f19e9dc
work <work@REDEMIRE>
2024-11-25 21:45:24 -0600

    Add project files.

---
+++ ValheimModNoCompass/MinimapPatch.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Minimap;
+
+namespace ValheimModNoCompass
+{
+    internal class MinimapPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Minimap), "DiscoverLocation")]
+        public static bool DiscoverLocationPrefix(ref UnityEngine.Vector3 pos, ref Minimap.PinType type, ref string name, ref bool showMap)
+        {
+            //orient the player to the location
+            if (type == Minimap.PinType.Boss)
+            {
+                Player.m_localPlayer.SetLookDir(pos - Player.m_localPlayer.transform.position, 3.5f);
+            }
+            //dont do anything else
+            return false;
+
+            //ref Minimap __instance
+            //var currentPins = __instance.m_playerPins; // Example property
+        }
+        //[HarmonyAfter]
+        //[HarmonyPatch(typeof(Minimap), "SetMapMode")]
+        //public static bool SetMapModePrefix(ref Minimap __instance)
+        //{
+        //    __instance.m_smallRoot.SetActive(value: false);
+        //        ModNoCompass.StaticLogger.LogInfo("Set Map mode after run change mini map to 'none'");
+        //    return true;
+        //}
+    }
+}
---
+++ ValheimModNoCompass/ModNoCompass.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ValheimModNoCompass
+{
+    [BepInPlugin("connorcacy.valheim.nocompass", "Valheim Mod for no compass and markers", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class ModNoCompass : BaseUnityPlugin
+    {
+        private readonly Harmony _harmony = new Harmony("connorcacy.valheim.nocompass");
+        public static ManualLogSource StaticLogger;
+        public void Awake()
+        {
+            StaticLogger = Logger;
+            Logger.LogInfo("Awake method called in no compass mod. Initializing mod...");
+            Harmony.DEBUG = true;
+            _harmony.PatchAll();
+        
+
+        }
+      
+    }
+}
